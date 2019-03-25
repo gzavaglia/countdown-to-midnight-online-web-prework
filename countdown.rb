81f97891cb7755b7e47ce305afbac7eecb4f02a8
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-25 20:45:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,12 +12,15 @@ end
 #returns"HAPPY NEW YEAR!"
 end
 
-#def countdown_with_sleep(input)
-#counter = input
-#while counter > 0
-#puts "#{counter} SECOND(S)!"
-#sleep(1.second)
-#counter -=1
-#end
-#puts "HAPPY NEW YEAR!"
-#end#
+def countdown_with_sleep(input)
+counter = input
+while counter >0
+puts "#{counter} SECOND(S)!"
+sleep(1.second)
+counter -= 1 
+if counter == 0 
+ return "HAPPY NEW YEAR!"
+end
+end
+#returns"HAPPY NEW YEAR!"
+end
