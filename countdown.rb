fc6f9963d0199adb1087aa0c5e0b69751703009b
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-25 20:37:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,4 +11,10 @@ end
 
 def countdown_with_sleep(input)
 counter = input
-while counter > =
+while counter > 0
+puts "#{counter} SECOND(S)!"
+sleep(1.second)
+counter -=1
+end
+puts "HAPPY NEW YEAR!"
+end
